95a318478004afb11ab57952262555491c7b9cce
Kaden Thomas <thomas.kaden4@outlook.com>
2018-07-13 01:02:54 -0700

    Add skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,24 +1,53 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import _ from "underscore";
+import classnames from "classnames";
 
 import "./skills.sass";
 
+export const SkillDot = ({ isOn }) =>
+    <div className={classnames("skill-dot", { "on": isOn })}>{isOn}</div>;
+
+SkillDot.propTypes = {
+    isOn: PropTypes.bool.isRequired
+};
+
+const MakeSkill = (name, level) => ({ name, level });
+
 const skills = [
-    "Go",
-    "Java",
-    "Linux"
+    MakeSkill("Rust", 4),
+    MakeSkill("Java", 8),
+    MakeSkill("Go", 9),
+    MakeSkill("C", 9),
+    MakeSkill("C++", 5),
+    MakeSkill("C#", 8)
 ];
 
-export const Skill = ({ length }) =>
+export const Skill = ({ name, length }) =>
     <div className="skill">
-        <div className="name">{skills[length]}</div>
-        <div className="bar"></div>
+        <div className="name">{name}</div>
+        <div className="dots">
+            {
+                _.range(0, 10).map(x =>
+                    <SkillDot isOn={x < length} />
+                )
+            }
+        </div>
     </div>;
+
+Skill.propTypes = {
+    name: PropTypes.string.isRequired,
+    length: PropTypes.number.isRequired,
+};
 
 export const Skills = () =>
     <div className="skills">
-        {_.range(0, 3).map(i => <Skill key={i} length={i} />)}
+        {
+            _(skills).map(({ name, level }, i) =>
+                <Skill key={i} name={name} length={level} />
+            )
+        }
     </div>;
 
 export default Skills;
